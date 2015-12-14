95e3c0f50bae0d3170fabc855d642e97b17be2c3
Huber Nicolas <nicolas@Hubers-MacBook-Pro.local>
2015-12-15 00:39:09 +0100

    all good

--- ex05/ft_is_sort.c
+++ ex05/ft_is_sort.c
@@ -6,24 +6,32 @@
 /*   By: nhuber <nhuber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/03 20:19:42 by nhuber            #+#    #+#             */
-/*   Updated: 2015/12/13 10:56:58 by nhuber           ###   ########.fr       */
+/*   Updated: 2015/12/15 00:37:59 by nhuber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_is_sort(int *tab, int length, int (*f)(int, int))
 {
 	int i;
+	int is_sort;
 
+	is_sort = 1;
 	i = 0;
-	if (length == 0)
-		return (0);
-	if (length == 1)
-		return (1);
-	while (i < length - 1)
+	while (i < length - 1 && is_sort == 1)
 	{
-		if (f(tab[i], tab[i + 1]) > 0)
-			return (0);
+		if (f(tab[i], tab[i + 1]) >= 0)
+			is_sort = 0;
 		i++;
 	}
-	return (1);
+	if (is_sort == 1)
+		return (1);
+	is_sort = 1;
+	i = 0;
+	while (i < length - 1 && is_sort == 1)
+	{
+		if (f(tab[i], tab[i + 1]) >= 0)
+			is_sort = 0;
+		i++;
+	}
+	return (is_sort);
 }
